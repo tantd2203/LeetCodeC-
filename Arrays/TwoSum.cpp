f9f88afc08c903dd52ffdb5a4b3fcc4d20787065
tan2203 <tranduytan.220300@gmail.com>
2025-08-11 17:03:43 +0700

    twosum

---
+++ Arrays/TwoSum.cpp
@@ -0,0 +1,22 @@
+
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+    int main() {
+        return 0;
+    }
+
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (map.find(complement) != map.end()) {
+                return {map.at(complement), i};
+            }
+            map.insert({nums[i], i});
+        }
+        return {};
+    }
+};
